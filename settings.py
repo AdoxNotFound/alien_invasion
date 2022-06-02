da5092389a305ef9f2ac1c4bad70d12cf1a455ae
Adrian Diez Canseco <adox3000@gmail.com>
2022-06-02 11:18:04 -0400

    cambio de color de background

--- settings.py
+++ settings.py
@@ -6,4 +6,4 @@ class Settings:
         # Screen settings
         self.screen_width = 1000
         self.screen_height = 600
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (102, 255, 255)
