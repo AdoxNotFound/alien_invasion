0ff44ffed0637a52dd7686a88d8bf8f79321b25e
Adrian Diez Canseco <adox3000@gmail.com>
2022-06-08 18:50:48 -0400

    Se da funcionalidad al boton de play

--- alien_invasion.py
+++ alien_invasion.py
@@ -54,7 +54,10 @@ class AlienInvasion:
             elif event.type == pygame.KEYDOWN:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                self._check_keyup_events(event)         
+                self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)         
 
     def _check_keydown_events(self, event):
         """Respond to keypresses."""
@@ -73,6 +76,11 @@ class AlienInvasion:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False 
+    
+    def _check_play_button(self, mouse_pos):
+        """Start a new game when the player clicks Play."""
+        if self.play_button.rect.collidepoint(mouse_pos):
+            self.stats.game_active = True
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
